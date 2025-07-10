eca0a6a112d798040fb5305d1b437c6dfbe59049
Roman Izegov <izegovrg@yandex.ru>
2025-07-10 11:45:45 +0300

    test database connection

---
+++ config/knex.js
@@ -0,0 +1,16 @@
+const knex = require('knex')
+require('dotenv').config()
+
+const db = knex({
+  client: "pg",
+  connection: {
+    host: process.env.PGHOST,
+    port: process.env.PGPORT,
+    user: process.env.PGUSER,
+    database: process.env.PGDATABASE,
+    password: process.env.PGPASSWORD,
+    ssl: {rejectUnauthorized: false}
+  }
+})
+
+module.exports =db
