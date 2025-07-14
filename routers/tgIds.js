25e664be57bb5b4028bebef8b3962e3a1ec9457b
Roman Izegov <izegovrg@yandex.ru>
2025-07-14 12:54:34 +0300

    updated ids

--- routers/tgIds.js
+++ routers/tgIds.js
@@ -4,9 +4,12 @@ require('dotenv').config();
 
 router.get("/chat-ids", (req, res) => {
   const chatIds = [
+    { name: "Empty", id: process.env.TG_ID_LEV },
     { name: "Roman", id: process.env.TG_ID_ROMAN },
     { name: "Tatiana", id: process.env.TG_ID_TATIANA },
     { name: "Lev", id: process.env.TG_ID_LEV },
+    { name: "Leonid", id: process.env.TG_ID_LEONID },
+
   ];
   res.json(chatIds);
 });
